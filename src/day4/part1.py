c104543ce0f353d274f5470428e04b524542fcd6
Jonathan Darrer <jdarrer@findmypast.com>
2024-12-04 08:59:53 +0000

    6 🚧 Add __main__ handler to solution

--- src/day4/part1.py
+++ src/day4/part1.py
@@ -1,2 +1,7 @@
 def calculate_result(input):
     return 0
+
+if __name__ == '__main__':
+    f = open("./src/day4/input.txt", "r")
+    input = f.read().split("\n")
+    print(calculate_result(input))
